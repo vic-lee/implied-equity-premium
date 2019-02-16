d9890d51ab7e56a47fe912f6ab77eba3e4755377
vic-lee <viclee@nyu.edu>
2019-02-15 21:16:56 -0500

    Get s&p 500 earnings and dividends data

---
+++ main.py
@@ -0,0 +1,46 @@
+from bs4 import BeautifulSoup
+from urllib import request as re 
+
+def soup_maker(url):
+	content = re.urlopen(url).read()
+	soup = BeautifulSoup(content, 'html.parser')
+	return soup
+
+
+def multpl_table_parser(url):
+	contents = {}
+	soup = soup_maker(url)
+	rows = soup.find_all('tr')
+	for row in rows:
+		yr = row.select('td.left')
+		data = row.select('td.right')
+		if len(yr) > 0 and len(data) > 0:
+			yr = yr[0].text[-4:]
+			try:
+				data = float(data[0].text.strip('\n'))
+			except: 
+				print("Error: data entry may not be a number.")
+				continue
+			contents[yr] = data
+	return contents
+
+
+def extract_annual_earnings(): 
+	earnings = multpl_table_parser("http://www.multpl.com/s-p-500-earnings/table")
+	return earnings
+
+
+def extract_annual_dividends():
+	dvds = multpl_table_parser("http://www.multpl.com/s-p-500-dividend/table")
+	return dvds
+	
+
+def main():
+	dividends = extract_annual_dividends()
+	print(dividends)
+	earnings = extract_annual_earnings()
+	print(earnings)
+
+
+if __name__ == '__main__':
+	main()
