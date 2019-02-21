e6611212ab26856809b41e403fd944677cfcf3c0
vic-lee <viclee@nyu.edu>
2019-02-20 22:25:25 -0500

    Initialize sp500 scraper

---
+++ index-data/sp500_data_extractor.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+
+
+def main():
+    bloomberg_sp500_url = 'https://www.bloomberg.com/quote/SPX:IND'
+    
+    pass
+
+
+if __name__ == '__main__':
+    main()
