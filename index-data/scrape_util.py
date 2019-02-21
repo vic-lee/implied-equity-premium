985fe0864745615e3a2db42ec830434b6360e3a1
vic-lee <viclee@nyu.edu>
2019-02-20 22:33:59 -0500

    Add comment to util function

--- index-data/scrape_util.py
+++ index-data/scrape_util.py
@@ -4,12 +4,17 @@ import csv
 
 
 def soup_maker(url):
+    """Returns a BeautifulSoup, given an url."""
     content = re.urlopen(url).read()
     soup = BeautifulSoup(content, 'html.parser')
     return soup
 
 
 def dict_to_csv(dict, filename):
+    """Write a dictionary to a new csv file, given a dict and a filename.
+    Note that if a file named `filename` already exists, this function 
+    will override that file without warning. 
+    """
     keys = dict.keys()
     with open(filename, 'w', newline='') as output_file:
         dict_writer = csv.DictWriter(output_file, keys)
