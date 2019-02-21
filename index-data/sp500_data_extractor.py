b40f14e4f5842b43f83b723efa1c006326def0b6
vic-lee <viclee@nyu.edu>
2019-02-20 23:50:54 -0500

    Refactor method to extract sp500

--- index-data/sp500_data_extractor.py
+++ index-data/sp500_data_extractor.py
@@ -4,17 +4,21 @@ import scrape_util as u
 from time import sleep
 
 
-def main():
+
+def extract_current_sp500():
     url = 'https://finance.yahoo.com/quote/%5EGSPC/'
     soup = u.soup_maker_webdriver(url) if not None else None
-
+    
     header = soup.find(name='div', attrs={'id': 'Lead-2-QuoteHeader-Proxy'})
     price = header.find(
         name='span', 
         attrs={'class': 'Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)'}
     )
-    
     print("S&P 500 Price: {}".format(price.text))
+
+
+def main():
+    extract_current_sp500()
 
 
 if __name__ == '__main__':
