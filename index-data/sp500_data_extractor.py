18e9141b8c0da1c189411999ce7987e9b74a38f3
vic-lee <viclee@nyu.edu>
2019-02-20 23:43:34 -0500

    Remove legacy scraping code

--- index-data/sp500_data_extractor.py
+++ index-data/sp500_data_extractor.py
@@ -7,27 +7,14 @@ from time import sleep
 def main():
     url = 'https://finance.yahoo.com/quote/%5EGSPC/'
 
-    # soup = u.soup_maker(url)
-    # price = soup.find(name='table', attrs={'class': 'performance-chart-table'})
-    # print(soup)
-    # print(price)
-
-    # Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)
     with webdriver.Chrome('_driver/chromedriver') as driver:
         driver.get(url)
-        # innerHTML = driver.execute_script("return document.body.innerHTML") 
-        # print(innerHTML)
-        html_source = driver.page_source
-        soup = BeautifulSoup(html_source, 'html.parser')
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-    # print(soup)
-    header = soup.find('div', attrs={'id': 'Lead-2-QuoteHeader-Proxy'})
-    print(header)
-    price = header.find('span', attrs={'class': 'Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)'}).text
+    header = soup.find(name='div', attrs={'id': 'Lead-2-QuoteHeader-Proxy'})
+    price = header.find(name='span', attrs={
+                        'class': 'Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)'}).text
     print("S&P 500 Price: {}".format(price))
-    # price = price_module.find_element_by_class_name('Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)')
-    # print(price)
-
 
 
 if __name__ == '__main__':
